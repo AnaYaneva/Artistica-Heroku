d8669f1e7cb2ed45d9084ff81e61a063f6e59bc8
Ana <ana_yaneva@abv.bg>
2022-08-11 15:55:57 +0300

    exception handling

--- src/main/java/com/artcources/artistica/exceptionHandling/ArtisticaControllerAdvice.java
+++ src/main/java/com/artcources/artistica/exceptionHandling/ArtisticaControllerAdvice.java
@@ -1,12 +1,36 @@
 package com.artcources.artistica.exceptionHandling;
 
+import com.artcources.artistica.exception.ResourceNotFoundException;
+import com.artcources.artistica.exception.UserNotFoundException;
+import com.artcources.artistica.exception.WorkshopNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ArtisticaControllerAdvice {
-   // @ExceptionHandler({Exception.class})
-    public String handleError() {
-        return "redirect:error";
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ModelAndView modelAndView(ResourceNotFoundException e){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("resource-not-found");
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ModelAndView modelAndView(UserNotFoundException e){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("user-not-found");
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(WorkshopNotFoundException.class)
+    public ModelAndView modelAndView(WorkshopNotFoundException e){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("workshop-not-found");
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        return modelAndView;
     }
 }
