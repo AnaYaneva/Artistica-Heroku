7fbc552e8561c7181e1ead55ac15846f66404cd1
Ana <ana_yaneva@abv.bg>
2022-08-14 17:42:23 +0300

    error handling

--- src/main/java/com/artcources/artistica/exceptionHandling/ArtisticaControllerAdvice.java
+++ src/main/java/com/artcources/artistica/exceptionHandling/ArtisticaControllerAdvice.java
@@ -4,17 +4,11 @@ import com.artcources.artistica.exception.ResourceNotFoundException;
 import com.artcources.artistica.exception.UserNotFoundException;
 import com.artcources.artistica.exception.WorkshopNotFoundException;
 import org.aspectj.lang.annotation.AfterThrowing;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
 import org.springframework.security.web.firewall.RequestRejectedException;
-import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ArtisticaControllerAdvice {
@@ -58,11 +52,11 @@ public class ArtisticaControllerAdvice {
         return modelAndView;
     }
 
-    @Bean
-    RequestRejectedHandler requestRejectedHandler() {
-        // sends an error response with a configurable status code (default is 400 BAD_REQUEST)
-        // we can pass a different value in the constructor
-        return new HttpStatusRequestRejectedHandler();
-    }
-
+//    @ExceptionHandler(RequestRejectedException.class)
+//    public ModelAndView modelAndView(RequestRejectedException e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("access-denied");
+//        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+//        return modelAndView;
+//    }
 }
